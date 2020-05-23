d49056c5f6497094f5b0558cf67b594776a542c0
Chaitanya <ckulkarni120@gmail.com>
2020-05-23 20:18:54 +0530

    changes

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ var express = require('express')
 var mongoose = require('mongoose')
 var session = require('express-session')
 var bodyParser = require("body-parser")
-// var multiparty = require('multiparty')
+var cors = require('cors')
 
 var app = express()
 
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 app.use(session({
